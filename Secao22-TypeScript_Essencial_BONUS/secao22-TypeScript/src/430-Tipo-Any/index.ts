937fdef8c1ca6e4d97e3df1e98362beff14e6bdf
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-19 15:39:26 -0300

    aula430

---
+++ Secao22-TypeScript_Essencial_BONUS/secao22-TypeScript/src/430-Tipo-Any/index.ts
@@ -0,0 +1,9 @@
+// any -> qualquer tipo (não é ideal) -> usar em último caso
+
+// por padrão já seria any -> mas para compilar sem erro precisa informar o tipo any
+function showMessage(msg: any) {
+  return msg;
+}
+
+showMessage('sla');
+showMessage(10);

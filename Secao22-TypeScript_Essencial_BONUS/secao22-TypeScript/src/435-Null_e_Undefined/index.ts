bf60037eb4347d6611e9a85a9b2f33103360ed1e
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-19 19:43:16 -0300

    aula435

---
+++ Secao22-TypeScript_Essencial_BONUS/secao22-TypeScript/src/435-Null_e_Undefined/index.ts
@@ -0,0 +1,34 @@
+// Undefined
+
+let x;
+console.log(x); // undefined
+
+function createPerson(
+  // parâmetros
+  firstName: string,
+  lastName?: string,
+): {
+  // tipagem do retorno
+  firstName: string;
+  lastName?: string;
+} {
+  // corpo da função
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+// quando o parâmetro é opicional -> retorna undefined
+console.log(createPerson('Vinicius')); // { firstName: 'Vinicius', lastName: undefined }
+
+// ------------------------------------------------------------
+// Null
+
+// é mais comum retorar/checar null em códigos:
+function squareOf(x: any) {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+console.log(squareOf('2')); // null
+console.log(squareOf(2)); // 4

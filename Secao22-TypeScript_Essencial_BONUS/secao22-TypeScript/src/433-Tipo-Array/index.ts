4b42fd0fac9cbb74ce0d4a8ddfb82272efd725f1
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-19 18:01:41 -0300

    aula433

---
+++ Secao22-TypeScript_Essencial_BONUS/secao22-TypeScript/src/433-Tipo-Array/index.ts
@@ -0,0 +1,22 @@
+// generic  /  type
+// Array<T> ou T[]
+
+function mult(...args: Array<number>): number {
+  return args.reduce((acc, value) => acc * value, 1);
+}
+const result = mult(1, 2, 3);
+console.log(result);
+
+// -------------------------------------------------
+function concat(...args: Array<string>): string {
+  return args.reduce((acc, value) => acc + value);
+}
+const result2 = concat('1', '2', '3');
+console.log(result2);
+
+// -------------------------------------------------
+function uCase(...args: string[]): string[] {
+  return args.map((value) => value.toUpperCase());
+}
+const result3 = uCase('a', 'b', 'c');
+console.log(result3);

1cff378138d91ccecc4f6419e689d45bc62d5a99
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-19 17:16:22 -0300

    aula432

---
+++ Secao22-TypeScript_Essencial_BONUS/secao22-TypeScript/src/432-Tipo-Object/index.ts
@@ -0,0 +1,45 @@
+// O MÉTODO MAIS RECOMENDADO:
+const objA = {
+  keyA: 'ValorA',
+  keyB: 'ValorB',
+};
+objA.keyA = 'NovoA';
+
+// Da erro pois essa chave não existe
+objA.keyC = 'ValorC';
+
+// -------------------------------------
+// existe uma forma de resolver isso:
+// mas não é recomendado, pois o vsCode não recomendará esses atributos como é normalmente
+const objB: Record<string, unknown> = {
+  keyA: 'ValorA',
+  keyB: 'ValorB',
+};
+objB.keyC = 'ValorC';
+
+// -------------------------------------
+// é possível tipar as chaves:
+const objC: {
+  keyA: string;
+  keyB: string;
+  keyC?: string; // ? -> opcional
+  [key: string]: unknown; // para receber outras chaves que ainda não existem (igual ao objB)
+} = {
+  keyA: 'ValorA',
+  keyB: 'ValorB',
+};
+
+objC.keyC = 'valorC';
+
+// -------------------------------------
+// para nunca alterar o valor da chave, apenas ler:
+const objD: {
+  readonly keyA: string;
+  readonly keyB: string;
+} = {
+  keyA: 'ValorA',
+  keyB: 'ValorB',
+};
+
+// não será possível atribuir outros valores
+objD.keyA = 'Sla';

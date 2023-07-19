ebae00277cc7cb553c43e66675af8c9dc0d6fccb
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-19 16:01:52 -0300

    aula431

---
+++ Secao22-TypeScript_Essencial_BONUS/secao22-TypeScript/src/431-Tipo-Void/index.ts
@@ -0,0 +1,18 @@
+// void -> função que não retorna nada
+// por padrão retorna undefined no JS
+
+function noReturn(...args: string[]): void {
+  console.log(args.join(' '));
+}
+noReturn('Vinicius', 'Santana');
+
+export const person = {
+  name: 'Vinicius',
+  lastName: 'Santana',
+
+  showName(): void {
+    console.log(this.name + ' ' + this.lastName);
+  },
+};
+
+person.showName();

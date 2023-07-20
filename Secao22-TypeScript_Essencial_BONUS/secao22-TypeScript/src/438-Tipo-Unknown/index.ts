dbbe8a14ef67677afe1238d22d5b832c88fc57cd
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-20 16:53:19 -0300

    aula438

---
+++ Secao22-TypeScript_Essencial_BONUS/secao22-TypeScript/src/438-Tipo-Unknown/index.ts
@@ -0,0 +1,23 @@
+// o 'pai' dos tipos -> vem antes de any na hierarqueia de tipos
+// seria um any mais 'seguro'
+
+// o any permite mudar de tipos sem dar avisos
+export let x: any;
+x = 10;
+x = 'Vinicius';
+x = true;
+x = 90;
+const y = 10;
+console.log(x + y);
+
+// já o unknown da um aviso
+export let a: unknown;
+a = 10;
+a = 'Vinicius';
+a = true;
+a = 90;
+const b = 10;
+console.log(a + b); // funciona do mesmo jeito, apenas da um aviso
+
+// é possivel fazer uma checagem
+if (typeof a === 'number') console.log(a + b);

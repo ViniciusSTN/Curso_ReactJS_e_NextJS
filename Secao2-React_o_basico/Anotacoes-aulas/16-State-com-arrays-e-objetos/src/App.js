e356131f6a36b17d77d9c46815b3552deab87319
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-22 16:23:06 -0300

    aula16

--- Secao2-React_o_basico/Anotacoes-aulas/16-State-com-arrays-e-objetos/src/App.js
+++ Secao2-React_o_basico/Anotacoes-aulas/16-State-com-arrays-e-objetos/src/App.js
@@ -3,30 +3,40 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    name: 'Vinicius',
-    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: 'Titulo 1',
+        body: 'Corpo 1',
+      },
+      {
+        id: 2,
+        title: 'Titulo 2',
+        body: 'Corpo 2',
+      },
+      {
+        id: 3,
+        title: 'Titulo 3',
+        body: 'Corpo 3',
+      }
+    ]
   };
 
-  // usar arrow function para não perder o this
-  handlePClick = () => {
-    this.setState({name: 'Joao'});
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  }
-
   render() {
-    const { name } = this.state;
-    const { counter } = this.state;
+    const { posts } = this.state;
 
     return (
-    <div className="App">
-      <header className="App-header">
-        <p onClick={this.handlePClick}>
-          {name} {counter}
-        </p>
-      </header>
-    </div>
-  );
+      <div className="App">
+        {
+          posts.map((post) => (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          ))
+        }
+      </div>
+    );
   }
 }
 

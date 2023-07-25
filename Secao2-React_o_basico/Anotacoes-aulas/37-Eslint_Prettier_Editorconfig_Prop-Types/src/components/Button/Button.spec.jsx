fea05cc4a21466377245ccd7db5a54474c68f71f
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-24 23:32:04 -0300

    aula37

---
+++ Secao2-React_o_basico/Anotacoes-aulas/37-Eslint_Prettier_Editorconfig_Prop-Types/src/components/Button/Button.spec.jsx
@@ -0,0 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Button } from '.';
+
+describe('<Button />', () => {
+  it('should render the button with the text "Load more"', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={false} onClick={fn} />);
+    expect.assertions(1); // espera ter exatamente uma assertions -> comum em async
+
+    const button = screen.getByRole('button', { name: /load more/i });
+    // expect(button).toHaveAttribute('class', 'sdgdfgd'); // importante ver o teste falhar para testar o test
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should call function on button click', () => {
+    const fn = jest.fn(); // mock para simular a função chamada no onlick
+    render(<Button text="Load more" disabled={false} onClick={fn} />);
+
+    const button = screen.getByRole('button', { name: /load more/i });
+
+    userEvent.click(button); // simula click no botão
+
+    expect(fn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should be disabled when disabled is true', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={true} onClick={fn} />);
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeDisabled();
+  });
+
+  it('should be enabled when disabled is false', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more" disabled={false} onClick={fn} />);
+    const button = screen.getByRole('button', { name: /load more/i });
+    expect(button).toBeEnabled();
+  });
+
+  it('should match snapshot', () => {
+    const fn = jest.fn();
+    const { container } = render(<Button text="Load more" disabled={false} onClick={fn} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
---
+++ Secao2-React_o_basico/Anotacoes-aulas/37-Eslint_Prettier_Editorconfig_Prop-Types/src/templates/Home/Home.spec.jsx
@@ -0,0 +1,6 @@
+describe('<Home />', () => {
+  // pode usar a palavra test ou it
+  it('is a dummy test', () => {
+    expect(1).toBe(1);
+  });
+});

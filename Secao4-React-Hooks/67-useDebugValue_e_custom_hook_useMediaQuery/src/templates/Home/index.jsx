ba943e37f330210d4bcb7abf0d1c563a09649ee0
ViniciusSTN <vinicius.stn@gmail.com>
2023-07-29 15:59:46 -0300

    aula67

---
+++ Secao4-React-Hooks/67-useDebugValue_e_custom_hook_useMediaQuery/src/templates/Home/index.jsx
@@ -0,0 +1,50 @@
+import { useDebugValue, useEffect, useState } from 'react';
+
+const useMediaQuery = (queryValue, initialValue = false) => {
+  const [match, setMatch] = useState(initialValue);
+
+  // é possivel ver na extensão do react no navegador
+  useDebugValue(`Query: ${queryValue}`, (name) => {
+    return name + ' modificado';
+  });
+
+  useEffect(() => {
+    let isMounted = true;
+    const matchMedia = window.matchMedia(queryValue);
+
+    const handleChange = () => {
+      if (!isMounted) return;
+      setMatch(Boolean(matchMedia.matches));
+    };
+
+    matchMedia.addEventListener('change', handleChange);
+    setMatch(!!matchMedia.matches);
+
+    return () => {
+      isMounted = false;
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [queryValue]);
+
+  return match;
+};
+
+const Home = () => {
+  const huge = useMediaQuery('(min-width: 980px)');
+  const big = useMediaQuery('(max-width: 979px) and (min-width: 768px)');
+  const medium = useMediaQuery('(max-width: 767px) and (min-width: 321px)');
+  const small = useMediaQuery('(max-width: 321px)');
+
+  const background = huge
+    ? 'green'
+    : big
+    ? 'red'
+    : medium
+    ? 'yellow'
+    : small
+    ? 'purple'
+    : null;
+  return <h1 style={{ fontSize: '60px', background }}>oi</h1>;
+};
+
+export default Home;

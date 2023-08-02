b9b75ea1969710762cf730a856eb6b04fbf77d7c
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-02 12:23:43 -0300

    aula116

---
+++ Secao8-Projeto1-Landing-Pages-com-react-Front-End/projeto/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { GlobalStyles } from './styles/global-styles';
+import Home from './templates/App/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <GlobalStyles />
+    <Home />
+  </>,
+);
---
+++ Secao8-Projeto1-Landing-Pages-com-react-Front-End/projeto/src/templates/App/App.jsx
@@ -0,0 +1,13 @@
+import * as Styled from './styles';
+
+function App() {
+  return (
+    <div className="App">
+      <Styled.Wrapper background="blue">
+        <h1>Hello</h1>
+      </Styled.Wrapper>
+    </div>
+  );
+}
+
+export default App;

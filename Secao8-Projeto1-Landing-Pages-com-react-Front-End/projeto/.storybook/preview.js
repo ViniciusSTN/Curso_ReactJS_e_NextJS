eece754b2194eb9f7bf8be699890f0952ca6448a
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-02 18:14:52 -0300

    aula123

--- Secao8-Projeto1-Landing-Pages-com-react-Front-End/projeto/.storybook/preview.js
+++ Secao8-Projeto1-Landing-Pages-com-react-Front-End/projeto/.storybook/preview.js
@@ -1,27 +1,29 @@
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from '../src/styles/global-styles';
-import { theme } from '../src/styles/theme';
+import { GlobalStyles } from '../src/styles/global-styles'
+import { theme } from '../src/styles/theme'
 
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.whiteColor,
       },
-    },
-  },
-};
+      {
+        name: 'dark',
+        value: theme.colors.darkColor,
+      },
+    ]
+  }
+}
 
 export const decorators = [
   (Story) => (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />
       <Story />
+      <GlobalStyles />
     </ThemeProvider>
   )
 ];
-
-export default preview;
--- Secao8-Projeto1-Landing-Pages-com-react-Front-End/projeto/src/styles/theme.js
+++ Secao8-Projeto1-Landing-Pages-com-react-Front-End/projeto/src/styles/theme.js
@@ -1,12 +1,7 @@
 export const theme = {
   colors: {
-    mainBg: '#202124',
-    secondaryBg: '#303134',
-    mediumGray: '#6A706E',
-    white: '#FFFBFE',
-    cyan: '#D9F7FA',
-    green: '#8FB339',
-    blue: '#00171F',
+    whiteColor: '#FFF',
+    darkColor: '#222425',
   },
 
   fonts: {},

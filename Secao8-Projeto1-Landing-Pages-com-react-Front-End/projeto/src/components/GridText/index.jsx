a005f98a998ef24f124699c3b0cc41064dc3a85b
ViniciusSTN <vinicius.stn@gmail.com>
2023-08-04 18:36:55 -0300

    aula145

---
+++ Secao8-Projeto1-Landing-Pages-com-react-Front-End/projeto/src/components/GridText/index.jsx
@@ -0,0 +1,37 @@
+import P from 'prop-types';
+import { Heading } from '../Heading';
+import { SectionBackground } from '../SectionBackground';
+import { TextComponent } from '../TextComponent';
+import * as Styled from './styles';
+
+export const GridText = ({ title, description, grid }) => {
+  return (
+    <SectionBackground>
+      <Styled.Container>
+        <Heading size="xlarge" uppercase>
+          {title}
+        </Heading>
+        <TextComponent>{description}</TextComponent>
+        <Styled.Grid>
+          {grid.map((el) => (
+            <Styled.GridElement key={el.title}>
+              <Heading size="medium">{el.title}</Heading>
+              <TextComponent>{el.description}</TextComponent>
+            </Styled.GridElement>
+          ))}
+        </Styled.Grid>
+      </Styled.Container>
+    </SectionBackground>
+  );
+};
+
+GridText.propTypes = {
+  title: P.string.isRequired,
+  description: P.string.isRequired,
+  grid: P.arrayOf(
+    P.shape({
+      title: P.string.isRequired,
+      description: P.string.isRequired,
+    }).isRequired,
+  ),
+};
